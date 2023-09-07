26527887c9dff001b5a2ce0c69c2a33cf7ec7254
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-09-07 22:06:03 +0200

    fix: home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -44,7 +44,7 @@ export default async (
             __html: css`
               @page {
                 size: auto;
-                margin: 1cm;
+                margin: 2cm 0.5cm;
 
                 @page :footer {
                   display: none;
@@ -61,6 +61,15 @@ export default async (
 
               @page :header {
                 display: none;
+              }
+
+              @media print {
+                .web {
+                  display: inline !important;
+                }
+                .print {
+                  display: none !important;
+                }
               }
             `,
           }}
@@ -92,6 +101,18 @@ export default async (
                     <div className="text-xs prose prose-xs ml-auto">
                       08 Septembre 2023
                     </div>
+                    <a
+                      href="https://esadhar.bureaudouble.com"
+                      className="hidden web text-xs underline block hover:(no-underline font-bold)"
+                    >
+                      Version en ligne
+                    </a>
+                    <a
+                      href="https://esadhar.storage.bureaudouble.com/application/esadhar_08092023.pdf"
+                      className="print text-xs underline block hover:(no-underline font-bold)"
+                    >
+                      Version pdf
+                    </a>
                   </div>
                 </Fragment>
               ))}
@@ -102,14 +123,20 @@ export default async (
                   .slice(0, index)
                   .reduce((p, v) => p + (v.images_json?.length ?? 0), 0);
                 let imageIndex = prevIndex;
+                const max = index > 0 && index !== arr.length - 2;
                 return (
-                  <div className="break-after-auto break-inside-avoid flex gap-8 relative before:(pointer-events-none aspect-square bg-yellow-400/[0.15] bg-clip-padding content-[' '] blur-[236px] absolute top-[90vh] w-full z-0 border-[12px] border-solid border-transparent)">
+                  <div className="break-after-auto break-inside-avoid grid grid-cols-[1fr_max-content_2.5fr] gap-8 relative before:(pointer-events-none aspect-square bg-yellow-400/[0.15] bg-clip-padding content-[' '] blur-[236px] absolute top-[90vh] w-full z-0 border-[12px] border-solid border-transparent)">
                     {index > 0 && index !== arr.length - 2 && (
-                      <div className="text-sm whitespace-nowrap font-bold col-span-full">
+                      <div className="text-sm whitespace-nowrap font-bold">
                         {post?.title}
                       </div>
                     )}
-                    <div className="rounded -mt-4 p-4 [&_p+div]:-mt-2 prose-p:text-justify prose-strong:font-medium [&_div+p]:mt-0 col-span-3 prose prose-black prose-h3:(font-normal uppercase text-xs)">
+                    <div
+                      className={clsx(
+                        !max ? "col-span-full" : "",
+                        "rounded -mt-4 p-4 [&_p+div]:-mt-2 prose-p:text-justify prose-strong:font-medium [&_div+p]:mt-0 prose prose-black prose-h3:(font-normal uppercase text-xs)"
+                      )}
+                    >
                       {createVNode({
                         html: post?.content,
                         h: (Type, args, children) => {
@@ -141,7 +168,7 @@ export default async (
                         },
                       })}
                     </div>
-                    <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-8 min-w-[20vw]">
                       {post.images_json?.map((d, index) => (
                         <a
                           className="flex flex-col gap-1 hover:underline"
@@ -164,15 +191,31 @@ export default async (
                 );
               })}
             </div>
-            <footer>
+            <footer className="prose prose-black max-w-4xl">
               Mise en page réalisée via des technologies web open-source, code
               hébergé sur{" "}
               <a
                 href="https://github.com/nestarz/esadhar-application"
-                className="underline font-bold"
+                className="underline font-bold whitespace-pre"
               >
                 github.com/nestarz/esadhar-application
               </a>
+              . <br />
+              La{" "}
+              <a
+                href="https://esadhar.storage.bureaudouble.com/application/esadhar_08092023.pdf"
+                className="underline hover:(no-underline font-bold)"
+              >
+                version PDF
+              </a>{" "}
+              a été généré à partir de la{" "}
+              <a
+                href="https://esadhar.bureaudouble.com"
+                className="underline hover:(no-underline font-bold)"
+              >
+                version en ligne
+              </a>{" "}
+              via la technologie CSS @print .
             </footer>
           </div>
         </div>
